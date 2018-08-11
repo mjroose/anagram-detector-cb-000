a619bb1b225193c3cc6a410a22aaa0b04d75dd9e
Michael Roose <mjroose@gmail.com>
2018-08-11 21:16:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(base_word)
+    @base_letters = base_word.split(//)
+  end
+
+  def match(words)
+    matches = []
+    words.each do |word|
+      letters = word.split(//)
+      matches << word if @base_letters.sort == letters.sort
+    end
+    matches
+  end
+end
