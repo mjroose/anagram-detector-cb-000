ecd804df8d1bce1a8e50b4b51872a3ab28a97be8
Michael Roose <mjroose@gmail.com>
2018-08-11 21:14:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,10 +2,15 @@
 class Anagram
 
   def initialize(base_word)
-    @base_word = base_word
+    @base_letters = base_word.split
   end
 
   def match(words)
-
+    matches = []
+    words.each do |word|
+      letters = word.split
+      matches << word if @base_letters.sort == letters.sort
+    end
+    words
   end
 end
